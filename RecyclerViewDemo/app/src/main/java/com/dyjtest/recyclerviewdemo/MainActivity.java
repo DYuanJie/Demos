5ffeaf124beea00d0b7273887db6c975c295afe7
dingyj <dingyj2017@163.com>
2017-09-14 23:23:30 +0800

    recyclerview 布局、点击事件、分割线、动画 http://blog.csdn.net/lmj623565791/article/details/45059587

--- RecyclerViewDemo/app/src/main/java/com/dyjtest/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/dyjtest/recyclerviewdemo/MainActivity.java
@@ -2,12 +2,85 @@ package com.dyjtest.recyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<String> mData;
+    private RecyclerView mainRv;
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initDatas();
+        mainRv = (RecyclerView) findViewById(R.id.main_rv);
+        myAdapter = new MyAdapter(mData);
+
+        //布局管理
+//        mainRv.setLayoutManager(new LinearLayoutManager(this));//线性
+//        mainRv.setLayoutManager(new GridLayoutManager(this, 4));//网格式
+        mainRv.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL));//瀑布流式
+
+        //adapter
+        mainRv.setAdapter(myAdapter);
+
+        //分割线
+        mainRv.addItemDecoration(new DividerGridItemDecoration(this));
+
+        //设置item动画
+        mainRv.setItemAnimator(new DefaultItemAnimator());
+
+        //点击监听
+        myAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, position + "click", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(MainActivity.this, position + "longclick", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add:
+                myAdapter.addData(1);
+                break;
+            case R.id.remove:
+                myAdapter.removeData(1);
+                break;
+        }
+        return true;
+    }
+
+    private void initDatas() {
+        mData = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++){
+            mData.add("" + (char)i);
+        }
     }
 }
---
+++ RecyclerViewDemo/app/src/main/java/com/dyjtest/recyclerviewdemo/MyAdapter.java
@@ -0,0 +1,94 @@
+package com.dyjtest.recyclerviewdemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by DingYuanJie on 2017/9/14.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+    private List<String> mDatas = null;
+
+    public MyAdapter(List<String> mDatas) {
+        this.mDatas = mDatas;
+    }
+
+    //点击事件的回调
+    public interface OnItemClickListener{
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view, int position);
+    }
+
+    private OnItemClickListener mOnItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener){
+        this.mOnItemClickListener = mOnItemClickListener;
+    }
+
+    //创建新的view
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    //绑定数据
+    @Override
+    public void onBindViewHolder(final MyAdapter.MyViewHolder holder, final int position) {
+        holder.wordTv.setText(mDatas.get(position));
+        holder.wordTv.setHeight((int) (Math.random()*100 + 150));
+
+        //如果设置了回调，则设置点击事件
+        if(mOnItemClickListener != null){
+            holder.wordTv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemClick(holder.itemView, pos);
+                }
+            });
+
+            holder.wordTv.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemLongClick(holder.itemView, pos);
+                    return false;
+                }
+            });
+        }
+    }
+
+    public void addData(int position){
+        mDatas.add(position, "Insert one");
+        //更新数据集
+        notifyItemInserted(position);
+    }
+
+    public void removeData(int position){
+        mDatas.remove(position);
+        //更新数据集
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    public class MyViewHolder extends ViewHolder {
+        private TextView wordTv;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            wordTv = (TextView) itemView.findViewById(R.id.word_tv);
+        }
+    }
+}
