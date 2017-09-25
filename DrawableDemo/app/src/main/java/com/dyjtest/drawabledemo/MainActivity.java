7410fb2a40f44ef41850131616cd86d348f491cf
DYuanJie <dingyj2017@163.com>
2017-09-25 17:27:23 +0800

    layer-list

--- DrawableDemo/app/src/main/java/com/dyjtest/drawabledemo/MainActivity.java
+++ DrawableDemo/app/src/main/java/com/dyjtest/drawabledemo/MainActivity.java
@@ -5,16 +5,17 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
-import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private MainTab01 mTab01;
     private MainTab02 mTab02;
+    private MainTab03 mTab03;
 
-    private LinearLayout mTabShape;
-    private LinearLayout mTabSelector;
+    private RadioButton radioButton0;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
 
     private FragmentManager fragmentManager;
 
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hideFragments(transaction);
         switch (index){
             case 0:
+                radioButton0.setChecked(true);
                 if(mTab01 == null) {
                     //如果为空，则创建一个并添加到界面上
                     mTab01 = new MainTab01();
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case 1:
+                radioButton1.setChecked(true);
                 if(mTab02 == null) {
                     //如果为空，则创建一个并添加到界面上
                     mTab02 = new MainTab02();
@@ -56,6 +59,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     transaction.show(mTab02);
                 }
                 break;
+            case 2:
+                radioButton2.setChecked(true);
+                if(mTab03 == null){
+                    mTab03 = new MainTab03();
+                    transaction.add(R.id.content, mTab03);
+                }else {
+                    transaction.show(mTab03);
+                }
         }
         transaction.commit();
     }
@@ -66,29 +77,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             transaction.hide(mTab01);
         }
         if(mTab02 != null) transaction.hide(mTab02);
+        if(mTab03 != null) transaction.hide(mTab03);
     }
 
     //清除所有选中状态
     private void resetBtn() {
+
+        radioButton0.setChecked(false);
+        radioButton1.setChecked(false);
+        radioButton2.setChecked(false);
     }
 
     private void initViews() {
-        mTabShape = (LinearLayout) findViewById(R.id.tab_btn_shape);
-        mTabSelector = (LinearLayout) findViewById(R.id.tab_btn_selector);
-        mTabShape.setOnClickListener(this);
-        mTabSelector.setOnClickListener(this);
+        radioButton0 = (RadioButton) findViewById(R.id.tab_btn0);
+        radioButton1 = (RadioButton) findViewById(R.id.tab_btn1);
+        radioButton2 = (RadioButton) findViewById(R.id.tab_btn2);
+        radioButton0.setOnClickListener(this);
+        radioButton1.setOnClickListener(this);
+        radioButton2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
 
         switch (view.getId()){
-            case R.id.tab_btn_shape:
+            case R.id.tab_btn0:
                 setTabSelection(0);
                 break;
-            case R.id.tab_btn_selector:
+            case R.id.tab_btn1:
                 setTabSelection(1);
                 break;
+            case R.id.tab_btn2:
+                setTabSelection(2);
             default:
                 break;
         }
--- DrawableDemo/app/src/main/java/com/dyjtest/drawabledemo/MainTab02.java
+++ DrawableDemo/app/src/main/java/com/dyjtest/drawabledemo/MainTab02.java
@@ -6,17 +6,46 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * selector样式
  * @author dingyj
  * @date 2017/9/23
  */
 
 public class MainTab02 extends Fragment{
+    private ListView mainTab02Lv;
+    private List<String> mData;
+    private ArrayAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maintab_02, container, false);
+        View view = inflater.inflate(R.layout.fragment_maintab_02, container, false);
+        mainTab02Lv = (ListView)view.findViewById(R.id.mainTab02Lv);
+        initData();
+
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_expandable_list_item_1);
+        adapter.addAll(mData);
+        mainTab02Lv.setAdapter(adapter);
+
+        return view;
+
     }
+
+
+    private void initData() {
+        mData = new ArrayList<>();
+        for (int i = 'a'; i < 'e'; i++){
+            mData.add("" + (char)i);
+        }
+    }
+
+
 }
